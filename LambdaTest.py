fed314386e79fc8956ca94194c9e20481185b0e6
ruiyuchen <ruiyu@MacBook-3.local>
2018-05-03 16:11:16 +0800

    jenkins test

--- LambdaTest.py
+++ LambdaTest.py
@@ -45,7 +45,7 @@ class PentiumDevTool(PentiumLocalToolClass):
     # ========== Developer MUST implement process function for each tool ==========
     def process(self):
         if self.isValid:
-            # connect to neo4j DB!
+            # connect to neo4j DB, jenkins job test
             driver = GraphDatabase.driver("bolt://34.215.102.35:7687", auth=("neo4j", "neo4j"))
             session = driver.session()
             session.run("CREATE (n:Pentium {Name: 'computer', Status: 'O'})")
